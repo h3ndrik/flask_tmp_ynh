e58b61bfe2aef5941f2851b2cf59071336f75e8e
Hendrik Langer <hendrik+dev@xd0.de>
2021-11-11 14:59:19 +0100

    fix substitutions

--- conf/gunicorn.py
+++ conf/gunicorn.py
@@ -1,11 +1,11 @@
 command = '__FINALPATH__/venv/bin/gunicorn'
 pythonpath = '__FINALPATH__'
 workers = 4
-user = '__YNH_APP_INSTANCE_NAME__'
+user = '__APP__'
 bind = 'unix:__FINALPATH__/sock'
-pid = '/run/gunicorn/__YNH_APP_INSTANCE_NAME__-pid'
-errorlog = '/var/log/__YNH_APP_INSTANCE_NAME__/error.log'
-accesslog = '/var/log/__YNH_APP_INSTANCE_NAME__/access.log'
+pid = '/run/gunicorn/__APP__-pid'
+errorlog = '/var/log/__APP__/error.log'
+accesslog = '/var/log/__APP__/access.log'
 access_log_format = '%({X-Real-IP}i)s %({X-Forwarded-For}i)s %(h)s %(l)s %(u)s %(t)s "%(r)s" %(s)s %(b)s "%(f)s" "%(a)s"'
 loglevel = 'warning'
 capture_output = True
