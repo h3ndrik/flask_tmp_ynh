485de9c4fd701490294460183031068cbf2f7b0b
Hendrik Langer <hendrik+dev@xd0.de>
2021-11-11 18:11:22 +0100

    flask_login

--- sources/app/__init__.py
+++ sources/app/__init__.py
@@ -1,7 +1,9 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
 
 db = SQLAlchemy()
+login_manager = LoginManager()
 
 def create_app():
 
@@ -14,5 +16,8 @@ def create_app():
     app.config['SQLALCHEMY_DATABASE_URI'] = SQLALCHEMY_DATABASE_URI
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     db.init_app(app)
+
+    login_manager.init_app(app)
+    from .auth import load_user_from_request
 
     return app
